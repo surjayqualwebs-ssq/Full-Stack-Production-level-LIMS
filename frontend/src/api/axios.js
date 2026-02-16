1f3cff3ba3679cbba0203015bd3f931ad74d75b3
surjayqualwebs-ssq <surjay.qualwebs@gmail.com>
2026-02-16 13:24:31 +0530

    fix: Making changes in axios.js, to identify frontend application

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: import.meta.env.VITE_API_URL || `${window.location.protocol}//${window.location.hostname}:3168`,
     headers: {
         'Content-Type': 'application/json',
     },
