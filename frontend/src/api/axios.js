331c4a547e3b1b93339682db087f0373f74a998e
surjayqualwebs-ssq <surjay.qualwebs@gmail.com>
2026-02-16 14:34:04 +0530

    Changing axios file

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || `${window.location.protocol}//${window.location.hostname}:3168`,
+    baseURL: '/api',
     headers: {
         'Content-Type': 'application/json',
     },
